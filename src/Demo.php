4da71e20d588cbb13eda5d7e2a8fb79133e9c128
chenyang <chenyang@qq.com>
2018-04-20 12:03:15 +0800

    测试

---
+++ src/Demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/4/20
+ * Time: 下午12:01
+ */
+
+namespace think;
+
+class Demo
+{
+    public function getDemo(){
+        return "String";
+    }
+}
